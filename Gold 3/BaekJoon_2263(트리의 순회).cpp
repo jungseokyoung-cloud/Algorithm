8b671e2a72cdf58d86e2fe0e9f7aac43a2316e92
jungseokyoung-cloud <jeongseog-yeong@jeongseog-yeong-ui-MacBook-Pro.local>
2021-07-24 20:02:56 +0900

    Gold 3

--- Gold 3/BaekJoon_2263(트리의 순회).cpp
+++ Gold 3/BaekJoon_2263(트리의 순회).cpp
@@ -1,56 +1,65 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define FAST ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
-#define MMAX 100000
+#define MMAX 100001
+int inOrder[MMAX];
+int FindIdx[MMAX];
+vector<int> postOrder;
 int N;
-vector<int> postorder;
-int inorder[MMAX];
-vector<int> preorder;
-void FindTree(int in_s, int in_e, int now){ // 1을 기준으로
-    if(postorder.size() == 0) return;
-   
-    int tar = postorder.back();
+vector<int> tree[MMAX];
+
+void DFS(int st, int en, int now){
+    if(st >= en) return;
+    if(postOrder.size() == 0) return;
     
-    for(int i = now + 1; i <= in_e; i++){
-        if(tar == inorder[i]){
-            postorder.pop_back();
-            FindTree(now, in_e, i);
-            preorder.push_back(inorder[i]);
-            break;
-        }
-    }
-    if(postorder.size() == 0) return;
-    tar = postorder.back();
-    
-    for(int i = in_s; i < now; i++){
-        if(tar == inorder[i]){
-            postorder.pop_back();
-            FindTree(in_s, now - 1, i);
-            preorder.push_back(inorder[i]);
-        }
+    for(int i = 0; i<2; i++){
+        if(postOrder.size() == 0) return;
+        
+        int target = postOrder.back();
+        int idx = FindIdx[target];
+        int now_idx = FindIdx[now];
+        
+        if(st > idx || en < idx) return;
+        postOrder.pop_back();
+        
+        tree[now].push_back(target);
+        if(now_idx < idx)   DFS(now_idx+1, en, target);
+        else DFS(st, now_idx-1, target);
     }
 }
+
+void Print(int node){
+    cout<<node<<' ';
+    if(tree[node].size() == 0)  return;
+    
+    for(int i = tree[node].size()-1; i>=0; i--){
+        int next = tree[node][i];
+        Print(next);
+    }
+}
+
 void Solve(){
-    for(int i = 0; i<N; i++){
-        if(postorder.back() == inorder[i]){
-            postorder.pop_back();
-            FindTree(0, N-1, i);
-            preorder.push_back(inorder[i]);
-            return;
-        }
-    }
+    int root = postOrder.back();
+    postOrder.pop_back();
+    DFS(1,N, root);
+    Print(root);
+    cout<<'\n';
 }
+
 
 int main(){
     FAST;
     cin>>N;
-    for(int i = 0; i<N; i++) cin>>inorder[i];
-    for(int i = 0; i<N; i++){
-        int N;
-        cin>>N;
-        postorder.push_back(N);
+    for(int i = 1; i<=N; i++){
+        cin>>inOrder[i];
+        FindIdx[inOrder[i]] = i;
     }
+    
+    for(int i = 1; i<=N; i++){
+        int num;
+        cin>>num;
+        postOrder.push_back(num);
+    }
+    
     Solve();
-    for(int i = preorder.size()-1; i>=0; i--) cout<<preorder[i]<<' ';
-    cout<<'\n';
 }
