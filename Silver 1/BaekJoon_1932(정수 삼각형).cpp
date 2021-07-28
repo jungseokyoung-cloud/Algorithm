bf56cde5797f41625ac0cca60b729e2b7049a1af
jungseokyoung-cloud <jeongseog-yeong@jeongseog-yeong-ui-MacBook-Pro.local>
2021-07-28 19:49:35 +0900

    Silver 1

--- Silver 1/BaekJoon_1932(정수 삼각형).cpp
+++ Silver 1/BaekJoon_1932(정수 삼각형).cpp
@@ -1,38 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define FAST ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
-#define MMAX 501
-int arr[MMAX][MMAX];
-int ans[MMAX][MMAX];
-int main()
-{
-    int Max=0,N;
-    FAST;
-    cin>>N;
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<=i; j++)
-            cin>>arr[i][j];
-    }
-    ans[0][0]=arr[0][0];
-    for(int i=1; i<N; i++)
-    {
-        for(int j=0; j<=i; j++)
-        {
-            if(j>0 && j<i)
-            {
-                ans[i][j]=max(ans[i-1][j], ans[i-1][j-1])+arr[i][j];
-                
-            }
-            else if(j==0)
-                ans[i][j]=ans[i-1][j]+arr[i][j];
-            else if(j==i)
-            {
-                ans[i][j]=ans[i-1][j-1]+arr[i][j];
-            }
-            if(Max<ans[i][j])
-                Max=ans[i][j];
+int arr[501][501];
+int ans[501][501];
+int N,res;
+
+void Solve(){
+    ans[1][1] = arr[1][1];
+    for(int i =1; i<N; i++){
+        for(int j = 1; j<=i; j++){
+            ans[i+1][j] = max(ans[i+1][j], ans[i][j] + arr[i+1][j]);
+            ans[i+1][j+1] = max(ans[i+1][j+1], ans[i][j] + arr[i+1][j+1]);
         }
     }
-    cout<<Max<<'\n';
+    
+    for(int i = 1; i<=N; i++) res = max(res, ans[N][i]);
+}
+
+int main(){
+    FAST;
+    cin>>N;
+    for(int i = 1; i<=N; i++){
+        for(int j =1; j<=i; j++){
+            cin>>arr[i][j];
+        }
+    }
+    Solve();
+    cout<<res<<'\n';
 }
