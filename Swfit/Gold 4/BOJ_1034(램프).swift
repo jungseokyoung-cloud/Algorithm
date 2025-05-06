a8a47d86d10f0de07e8f2a3882f55e770f2cca9b
Jung SeokYoung <thrdud0423@gmail.com>
2025-05-06 17:29:17 +0900

    2025.05.06

--- Swfit/Gold 4/BOJ_1034(램프).swift
+++ Swfit/Gold 4/BOJ_1034(램프).swift
@@ -1,50 +1,44 @@
 func solution() {
-  let (n, _) = readPairInt()
+  let (n, _) = readPairIntegers()
   let map = configureMap(n)
-  let k = readInt()
-  let result = maxTurnOnColumn(map, k: k)
-  print(result)
-}
-
-func readInt() -> Int {
-  return Int(readLine()!)!
-}
-
-func readPairInt() -> (Int, Int) {
-  let inputs = readLine()!.split(separator: " ").map { Int($0)! }
+  let orderBySameCol = orderdBySameCol(map)
+  let k = readInteger()
   
-  return (inputs[0], inputs[1])
+  let result = orderBySameCol
+    .filter { (key, value) -> Bool in
+      let zeroCount = key.filter { $0 == "0" }.count
+      return zeroCount <= k && zeroCount % 2 == k % 2
+    }
+    .map { $0.value }
+    .sorted(by: >)
+  print(result.first ?? 0)
 }
 
 func configureMap(_ n: Int) -> [String] {
   var map = [String]()
   
-  for _ in 0..<n {
-    map.append(readLine()!)
-  }
+  for _ in 0..<n { map.append(readLine()!) }
   
   return map
 }
 
-func maxTurnOnColumn(_ map: [String], k: Int) -> Int {
-  var colZeroCount = Array(repeating: 0, count: map.count)
-  var result = 0
+func orderdBySameCol(_ map: [String]) -> [String: Int] {
+  var results = [String: Int]()
   
-  for (index, mapRow) in map.enumerated() {
-    let zeroCount = mapRow.filter { $0 == "0" }.count
-    
-    colZeroCount[index] = zeroCount
+  for col in map {
+    results[col, default: 0] += 1
   }
   
-  for index in 0..<map.count {
-    let zeroCount = colZeroCount[index]
-    guard zeroCount <= k, zeroCount % 2 == k % 2 else { continue }
-    
-    let count = map.filter { map[index] == $0 }.count
-    result = max(result, count)
-  }
-  
-  return result
+  return results
+}
+
+func readPairIntegers() -> (Int, Int) {
+  let inputs = readLine()!.split(separator: " ").map { Int($0)! }
+  return (inputs[0], inputs[1])
+}
+
+func readInteger() -> Int {
+  return Int(readLine()!)!
 }
 
 solution()
