18692932ab02febbe6619f7036ec20bdce9dda7e
Jung SeokYoung <thrdud0423@gmail.com>
2025-05-08 12:08:58 +0900

    2025.05.08

--- Swfit/Gold 2/BOJ_7570(줄 세우기).swift
+++ Swfit/Gold 2/BOJ_7570(줄 세우기).swift
@@ -1,30 +1,22 @@
-//
-//  BOJ_7570(줄 세우기).swift
-//  BOJ2
-//
-//  Created by jung on 7/8/24.
-//
-
 func solution() {
-	let n = readInterger()
-	let array = readIntergers()
-	var dp = Array(repeating: 0, count: n+1)
-	var result = 0
-	
-	for element in array {
-		dp[element] = dp[element-1] + 1
-		result = max(result, dp[element])
-	}
-	
-	print(n - result)
+  let n = readInteger()
+  let persons = readIntegers()
+  var lengthes = Array(repeating: 0, count: n + 1)
+  
+  for person in persons {
+    lengthes[person] = lengthes[person - 1] + 1
+  }
+  
+  let temp = lengthes.max() ?? 0
+  print(n - temp)
 }
 
-func readInterger() -> Int {
-	return Int(readLine()!)!
+func readInteger() -> Int {
+  return Int(readLine()!)!
 }
 
-func readIntergers() -> [Int] {
-	return readLine()!.split(separator: " ").map { Int($0)! }
+func readIntegers() -> [Int] {
+  return readLine()!.split(separator: " ").map { Int($0)! }
 }
 
 solution()
